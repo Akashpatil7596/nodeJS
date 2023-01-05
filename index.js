bdf4ec6f11d95ef1d753ac02e06283f3cc74b15f
Akashpatil7596 <99664587+Akashpatil7596@users.noreply.github.com>
2023-01-05 18:15:56 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 "use strict";
 
-// File System Module (Blocking Synchronous Way)
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
 // File System //
 
-
-// const text = fs.readFileSync('demo.txt', 'utf-8');
+//Synchronous Way
+// const text = fs.readFileSync('./fs/demo.txt', 'utf-8');
 // console.log(text);
 
 // const textUpload = `This Is What We Know About Avocado🥑 ${text}.\n CreatedOn ${Date.now()}`;
-// fs.writeFileSync('demo.text', textUpload);
+// fs.writeFileSync('./fs/demo.text', textUpload);
 // console.log("File Uploaded");
-
+// 
 // Asynchronous Way
-// fs.readFile('start.txt','utf-8' , (err, data1) => {
+// fs.readFile('.fs/start.txt','utf-8' , (err, data1) => {
 //     if(err) return console.log("ERROR! 💣");
 
 //     fs.readFile(`${data1}.txt`,'utf-8' , (err, data2) => {
 //         console.log(data2);
-//         fs.readFile('append.txt' ,'utf-8', (err, data3) =>{
+//         fs.readFile('./fs/append.txt' ,'utf-8', (err, data3) =>{
 //             console.log(data3)
-//             fs.writeFile('.final.txt',`${data2}\n${data3}`,'utf-8', err => {
+//             fs.writeFile('./fs/final.txt',`${data2}\n${data3}`,'utf-8', err => {
 //                 console.log("Your File Uploaded 🍔");
 //             })
 //         });
